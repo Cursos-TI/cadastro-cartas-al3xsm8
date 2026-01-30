b99e6823d43f322d819115cc5592673ce797e70d
Alex Barbosa <101755039+al3xsm8@users.noreply.github.com>
2026-01-30 15:46:00 +0000

    Update - nivel aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,48 +7,97 @@
 
 int main() {
     // Área para definição das variáveis para armazenar as propriedades das cidades
-    char estado;
-    char codigo_estado[3], cidade[15];
-    int populacao, pontos_turisticos;
-    float area, pib;
+    char state, state_2;
+    char code[4], code_2[4];
+    char city[20], city_2[20];
+    int population, population_2;
+    float area, area_2;
+    double pib, pib_2;
+    int tourist_points, tourist_points_2;
+    float density, density_2;
+    double pib_per, pib_per_2;
 
-    // Área para entrada de dados
-    printf("CADASTRO CARTA\n");
-    printf("Estado da carta: ");
-    scanf(" %c", &estado);
+    // Área para entrada de dados carta 1
+    printf("\nCadastro carta 1: \n");
+    printf("Estado da carta: \n");
+    scanf(" %c", &state);
 
-    printf("Codigo da carta: ");
-    scanf("%s", &codigo_estado);
+    printf("Codigo da carta: \n");
+    scanf("%s", &code);
 
-    printf("Nome da cidade: ");
-    scanf("%s", &cidade);
+    printf("Cidade: \n");
+    scanf("%s", &city);
 
-    printf("Numero de habitantes: ");
-    scanf("%d", &populacao);
+    printf("Populacao: \n");
+    scanf("%d", &population);
 
-    printf("Area: ");
+    printf("Area: \n");
     scanf("%f", &area);
 
-    printf("Pib: ");
-    scanf("%f", &pib);
+    printf("Pib: \n");
+    scanf("%lf", &pib);
 
-    printf("Pontos turisticos: ");
-    scanf("%d", &pontos_turisticos);
+    printf("Pontos Turisticos: \n");
+    scanf("%d", &tourist_points);
 
-    system("clear");
+
+    density = population / area;
+    pib_per = (pib * 1000000000) / population;
+
+    // Área para entrada de dados carta 2
+    printf("\nCadastro carta 2: \n");
+    printf("Estado da carta: \n");
+    scanf(" %c", &state_2);
+
+    printf("Codigo da carta: \n");
+    scanf("%s", &code_2);
+
+    printf("Cidade: \n");
+    scanf("%s", &city_2);
+
+    printf("Populacao: \n");
+    scanf("%d", &population_2);
+
+    printf("Area: \n");
+    scanf("%f", &area_2);
+
+    printf("Pib: \n");
+    scanf("%lf", &pib_2);
+
+    printf("Pontos Turisticos: \n");
+    scanf("%d", &tourist_points_2);
+
+
+    density_2 = population_2 / area_2;
+    pib_per_2 = (pib_2 * 1000000000) / population_2;
+
+    //Comando para limpar terminal apos input
+    system("clear"); //(cls) == windows, (clear) == linux
     printf("\n");
    
-    // Área para exibição dos dados da cidade
-    printf("-------------CARTA 01-------------\n");
-    printf("- Estado: %c\n", estado);
-    printf("- Codigo do estado: %s\n", codigo_estado);
-    printf("- Cidade: %s\n", cidade);
-    printf("- Populacao: %d\n");
-    printf("- Area: %f\n", area);
-    printf("- PIB: %f\n", pib);
-    printf("- Numero de pontos turisticos: %d\n", pontos_turisticos);
-    printf("----------------------------------\n\n");
+    // Área para exibição dos dados da cidade carta 1
+    printf("Carta 1:\n");
+    printf("Estado: %c\n", state);
+    printf("Codigo: %s\n", code);
+    printf("Nome da Cidade: %s\n", city);
+    printf("Populacao: %d\n", population);
+    printf("Area: %.2f km2\n", area);
+    printf("PIB %.2f bilhoes de reais\n", pib);
+    printf("Numero de Pontos Turisticos: %d\n", tourist_points);
+    printf("Densidade Populacional: %.2f hab/km2\n", density);
+    printf("PIB per Capita: %.2f reais\n\n", pib_per);
 
+    // Área para exibição dos dados da cidade carta 2
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", state_2);
+    printf("Codigo: %s\n", code_2);
+    printf("Nome da Cidade: %s\n", city_2);
+    printf("Populacao: %d\n", population_2);
+    printf("Area: %.2f km2\n", area_2);
+    printf("PIB %.2f bilhoes de reais\n", pib_2);
+    printf("Numero de Pontos Turisticos: %d\n", tourist_points_2);
+    printf("Densidade Populacional: %.2f hab/km2\n", density_2);
+    printf("PIB per Capita: %.2f reais\n", pib_per_2);
     
-return 0;
+    return 0;
 } 
